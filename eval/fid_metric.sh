092ad3e65a14e96cb933f4f7cda790d99197ee82
Pedro Sousa <sousa.pedro.fernandes@gmail.com>
2024-02-06 14:22:02 +0000

    FID Evaluation Metric (06/02/2024)

---
+++ eval/fid_metric.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min11GB             # Partition (check with "$sinfo")
+#SBATCH --output=fid_output.out         # Filename with STDOUT. You can use special flags, such as %N and %j.
+#SBATCH --error=fid_error.out           # (Optional) Filename with STDERR. If ommited, use STDOUT.
+#SBATCH --job-name=metabrest                # (Optional) Job name
+#SBATCH --time=14-00:00                     # (Optional) Time limit (D: days, HH: hours, MM: minutes)
+#SBATCH --qos=gpu_min11GB                   # (Optional) 01.ctm-deep-05
+
+#Commands / scripts to run (e.g., python3 train.py)
+python3 fid_metric.py
